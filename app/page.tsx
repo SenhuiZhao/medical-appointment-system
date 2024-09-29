1bb5a40f3b7754a37b3c7d14ea140b2daf75583a
SenhuiZhao <zhaosky12187@gmail.com>
2024-09-28 22:37:07 -0400

    globals css & wailwind config set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-3xl underline">
+    <div className="text-white text-3xl underline">
       <h1>Hi, Medical system</h1>
+      <Button>click me</Button>
     </div>
   );
 }
